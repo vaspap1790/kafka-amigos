d34de0b52634f8cff2752be337f4c63fa401d569
vasilis.papadimitrakopoulos <vasilis.papadimitrakopoulos@sword-group.com>
2022-06-14 15:16:18 +0300

    set up

---
+++ src/main/java/com/vaspap/kafkaamigoscode/KafkaAmigoscodeApplication.java
@@ -0,0 +1,13 @@
+package com.vaspap.kafkaamigoscode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaAmigoscodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaAmigoscodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vaspap/kafkaamigoscode/KafkaAmigoscodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vaspap.kafkaamigoscode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaAmigoscodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
